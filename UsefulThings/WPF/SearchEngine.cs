f361ac90aa41f7b6459202a4813207010415669b
KFreon <kfreon@hotmail.com>
2015-03-11 20:57:19 +1000

    Added SearchEngine

---
+++ UsefulThings/WPF/SearchEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulThings.WPF
+{
+    public class SearchEngine<T> where T : new()
+    {
+        public ICollection<T> SearchableCollection { get; set; }
+        private Dictionary<string, Func<T, string, bool>> SearchMethods { get; set; }
+
+        public SearchEngine(ICollection<T> searchable, params KeyValuePair<string, Func<T, string, bool>>[] searchers)
+        {
+            SearchableCollection = searchable;
+            SearchMethods = new Dictionary<string, Func<T, string, bool>>();
+            foreach (var searcher in searchers)
+                SearchMethods.Add(searcher.Key, searcher.Value);
+        }
+
+        public List<T> Search(string searchString, string searchMethodKey = null, ICollection<T> collection = null)
+        {
+            List<T> results = new List<T>();
+            ICollection<T> SearchGroup = collection ?? SearchableCollection;
+            Func<T, string, bool> Searcher = searchMethodKey == null ? SearchMethods.First().Value : SearchMethods[searchMethodKey];
+
+            foreach (T item in SearchGroup)
+            {
+                if (Searcher(item, searchString))
+                    results.Add(item);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ UsefulThings/WPF/SearchEngineViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulThings.WPF
+{
+    public class SearchViewModelBase<T> : ViewModelBase where T : new()
+    {
+	    protected SearchEngine<T> searchEngine {get;set;}
+
+	    string searchbox1 = null;
+	    public virtual string SearchBoxText1 
+	    {
+		    get
+		    {
+			    return searchbox1;
+		    }
+		    set 
+		    {
+			    SetProperty(ref searchbox1, value);
+			    searchEngine.Search(value);
+		    }
+	    }
+
+        public SearchViewModelBase(ICollection<T> searchingCollection, params KeyValuePair<string, Func<T, string, bool>>[] Searchers)
+            : base()
+	    {
+		    searchEngine = new SearchEngine<T>(searchingCollection, Searchers);
+	    }
+    }
+}
