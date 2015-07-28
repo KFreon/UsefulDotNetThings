b2daa3114bc27e0a35ce12ab8c092069070d6950
Freon <KFreon@hotmail.com>
2015-07-28 04:22:24 +0000

    Updated EnumToStringConverter.cs

--- UsefulThings/WPF/EnumToStringConverter.cs
+++ UsefulThings/WPF/EnumToStringConverter.cs
@@ -13,7 +13,7 @@ namespace UsefulThings.WPF
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if(parameter == null || value == null)
-                throw new ArgumentNullException("Both parameter and value must be specified.");
+                return null;
             
             // KFreon: Try to convert the value of an enum to a string.
             Type parameterType = (Type)parameter;
@@ -28,7 +28,7 @@ namespace UsefulThings.WPF
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if (value == null || parameter == null)
-                throw new ArgumentNullException("Both paramter and value must be specified.");
+                return null;
             
             // KFreon: Try to convert string to enum.
             Type parameterType = (Type)parameter;
