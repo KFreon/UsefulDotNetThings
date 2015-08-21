857945b7f406a3842b55ee783bda64db7ceda229
Freon <kfreon@hotmail.com>
2015-08-21 16:18:28 +1000

    Added WPF TreeView that allows virtualising AND random seeking/searching. Any root element can be brought into view.

---
+++ UsefulThings/WPF/SeekableVirtualTreeView.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace UsefulThings.WPF
+{
+    public class SeekableVirtualTreeView : TreeView
+    {
+        ScrollViewer scroller = null;
+        double ItemHeight = 0;
+
+
+        private int Test(IEnumerator enumerator, object desiredItem)
+        {
+            int count = 0;
+            while (enumerator.MoveNext())
+            {
+                if (enumerator.Current == desiredItem)
+                    break;
+
+                var seekable = enumerator.Current as ITreeSeekable;
+                if (seekable.IsExpanded)
+                    count += Test(seekable.ChildEnumerator, desiredItem);
+                else
+                    count++;
+            }
+
+            return count;
+        }
+
+        public void BringItemIntoView(object item)
+        {
+            if (ItemsSource == null)
+                return;
+
+            // Get item height - height should be same for all of them
+            if (ItemHeight == 0)
+            {
+                var randomVisibleContainer = ItemContainerGenerator.Items[0];
+                var actualTreeItem = (TreeViewItem)ItemContainerGenerator.ContainerFromItem(randomVisibleContainer);
+                ItemHeight = actualTreeItem.ActualHeight;
+            }
+
+
+            // Find the index of the item, and account for expanded items "above", they'll take up space
+            var enumerator = ItemsSource.GetEnumerator();
+
+            int itemIndex = Test(enumerator, item);
+
+            if (itemIndex == -1)
+                throw new InvalidOperationException($"Item {item} not found in ItemsSource");
+
+
+            if (scroller == null)
+                scroller = this.Template.FindName("_tv_scrollviewer_", this) as ScrollViewer;
+
+            
+
+
+            if (scroller != null)
+            {
+                // Get scrolling extent - this is the "size" of the scrollbar
+                // This is going to need to take into account all expanded items as well. Might already?
+                var extentHeight = scroller.ExtentHeight;
+
+                double test = extentHeight / ItemHeight;  // should be the number of elements?
+                double desiredOffset = ItemHeight * itemIndex - (ActualHeight / 2);
+
+                if (desiredOffset < 0)
+                    desiredOffset = 0;
+
+                scroller.ScrollToVerticalOffset(desiredOffset);
+                scroller.UpdateLayout();
+            }
+        }
+    }
+}
